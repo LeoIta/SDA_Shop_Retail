3dca497debb09091630ab68cb89bd545c6e87e2d
Leo <leonardolorusso83@gmail.com>
2020-11-01 21:12:29 +0100

    add Test values in the tables

--- SDAOnlineShop/src/test/java/repository/TestAddressRepository.java
+++ SDAOnlineShop/src/test/java/repository/TestAddressRepository.java
@@ -9,5 +9,14 @@ public class TestAddressRepository {
     public Address address;
 
     @BeforeEach
-    public void start() { address = new Address(1,"Italy","Rome","00186","Via del Corso 114/115");}
+    public void start() { address = new Address(3,"Italy","Rome","00186","Via del Corso 114/115");}
+
+
+
+
+
+
+
+
+
 }

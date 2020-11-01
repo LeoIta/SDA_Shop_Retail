63145456c4c952f3b25f05a7c4e59c3bf8fb3f49
cheuwami <67438440+cheuwami@users.noreply.github.com>
2020-11-01 02:48:00 +0200

    Add files via upload

---
+++ SDAOnlineShop/src/main/java/repository/ProductRepository.java
@@ -0,0 +1,128 @@
+package repository;
+
+import model.Login;
+import model.Product;
+import util.DBUtil;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductRepository {
+
+    public static List<Product> findAll() {
+        List<Product> productList = new ArrayList<Product>();
+        String selectAll = "SELECT * FROM product";
+        try {
+            Connection connection = DBUtil.newConnection();
+            PreparedStatement pstmt = connection.prepareStatement(selectAll);
+            ResultSet rs = pstmt.executeQuery(selectAll);
+
+            while(rs.next()) {
+
+                int productId = rs.getInt(1);
+                String color = rs.getString(2);
+                String size = rs.getString(3);
+                String productCode = rs.getString(4);
+                int price = rs.getInt(5);
+                int orderId = rs.getInt(6);
+
+                Product product = new Product(productId, color, size,productCode,price);
+                productList.add(product);
+            }
+            rs.close();
+            pstmt.close();
+            connection.close();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+        return productList;
+    }
+
+    public static List<Product> findById(int id) {
+        List<Product> productList = new ArrayList<Product>();
+        String selectById = "SELECT * FROM product where productId=?";
+
+        try {
+            Connection connection = DBUtil.newConnection();
+            PreparedStatement pstmt = connection.prepareStatement(selectById);
+            //pstmt.setString(1,login.getAccountId());
+            pstmt.setInt(1,id);
+            ResultSet rs = pstmt.executeQuery(selectById);
+            while(rs.next()) {
+
+                int productId = rs.getInt(1);
+                String color = rs.getString(2);
+                String size = rs.getString(3);
+                String productCode = rs.getString(4);
+                int price = rs.getInt(5);
+                int orderId = rs.getInt(6);
+
+                Product product = new Product(productId, color, size, productCode, price);
+                productList.add(product);
+            }
+            rs.close();
+            pstmt.close();
+            connection.close();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return productList;
+    }
+
+    public static void deleteLoginById(int id) {
+        String deleteById = "DELETE FROM product where productId=?";
+        try {
+            Connection connection = DBUtil.newConnection();
+            PreparedStatement pstmt = connection.prepareStatement(deleteById);
+            pstmt.setInt(1,id);
+            int deletedRecords = pstmt.executeUpdate(deleteById);
+            pstmt.close();
+            connection.close();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+    public static void saveNewProduct(Product product) {
+        String newProduct = "INSERT INTO Login VALUES(null,?,?,?,?)";
+        try {
+            Connection connection = DBUtil.newConnection();
+            PreparedStatement pstmt = connection.prepareStatement(newProduct);
+            pstmt.setString(1,product.getColor());
+            pstmt.setString(2,product.getSize());
+            pstmt.setString(2,product.getProductCode());
+            pstmt.setInt(2,product.getPrice());
+
+            int newRecords = pstmt.executeUpdate(newProduct);
+            pstmt.close();
+            connection.close();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+    public static void updateLoginById(Product product) {
+        String updateProduct = "UPDATE Login SET color=?,size=?,productCode=?,price=?, where productId=?";
+        try {
+            Connection connection = DBUtil.newConnection();
+            PreparedStatement pstmt = connection.prepareStatement(updateProduct);
+
+            pstmt.setString(1,product.getColor());
+            pstmt.setString(2,product.getSize());
+            pstmt.setString(3,product.getProductCode());
+            pstmt.setInt(4,product.getPrice());
+            pstmt.setInt(5,product.getProductID());
+
+            int newRecords = pstmt.executeUpdate(updateProduct);
+            pstmt.close();
+            connection.close();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+}
---
+++ SDAOnlineShop/src/main/java/repository/StorageRepository.java
@@ -0,0 +1,112 @@
+package repository;
+
+import model.Login;
+import model.Storage;
+import util.DBUtil;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class StorageRepository {
+    public static List<Storage> findAll() {
+        List<Storage> storageList = new ArrayList<Storage>();
+        String selectAll = "SELECT * FROM storage";
+        try {
+            Connection connection = DBUtil.newConnection();
+            PreparedStatement pstmt = connection.prepareStatement(selectAll);
+            ResultSet rs = pstmt.executeQuery(selectAll);
+
+            while(rs.next()) {
+                String userName = rs.getString(1);
+                int quantity = rs.getInt(2);
+
+                Storage storage = new Storage( userName, quantity);
+                storageList.add(storage);
+            }
+            rs.close();
+            pstmt.close();
+            connection.close();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return storageList;
+    }
+
+    public static List<Storage> findByCode(String code) {
+        List<Storage> storageList = new ArrayList<Storage>();
+        String selectByCode = "SELECT * FROM storage where productCode=?";
+
+        try {
+            Connection connection = DBUtil.newConnection();
+            PreparedStatement pstmt = connection.prepareStatement(selectByCode);
+            pstmt.setString(1,code);
+            ResultSet rs = pstmt.executeQuery(selectByCode);
+            while(rs.next()) {
+                String productCode = rs.getString(1);
+                int quantity = rs.getInt(2);
+
+                Storage storage = new Storage(productCode, quantity);
+                storageList.add(storage);
+            }
+            rs.close();
+            pstmt.close();
+            connection.close();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return storageList;
+    }
+
+    public static void deleteStorageByCode(String code) { // wont be needed
+        String deleteByCode = "DELETE FROM storage where code=?";
+        try {
+            Connection connection = DBUtil.newConnection();
+            PreparedStatement pstmt = connection.prepareStatement(deleteByCode);
+            pstmt.setString(1,code);
+            int deletedRecords = pstmt.executeUpdate(deleteByCode);
+            pstmt.close();
+            connection.close();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+    public static void saveNewStorage(Storage storage) { // wont be needed. we will populate the database ourselves
+        String saveNewStorage = "INSERT INTO storage VALUES(?,?)";
+        try {
+            Connection connection = DBUtil.newConnection();
+            PreparedStatement pstmt = connection.prepareStatement(saveNewStorage);
+            pstmt.setString(1,storage.getProductCode());
+            pstmt.setInt(2,storage.getAvailable_quantity());
+
+            int newRecords = pstmt.executeUpdate(saveNewStorage);
+            pstmt.close();
+            connection.close();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+    public static void updateStorageByCode(String code, int soldQty) {
+        List<Storage> storageList = findByCode(code);
+        int newAvailableQty = storageList.get(1).getAvailable_quantity() - soldQty;
+        String updateStorage = "UPDATE storage SET available_quantity=?, where productCode=?";
+        try {
+            Connection connection = DBUtil.newConnection();
+            PreparedStatement pstmt = connection.prepareStatement(updateStorage);
+
+            pstmt.setInt(1,newAvailableQty);
+            pstmt.setString(2,code);
+
+            int newRecords = pstmt.executeUpdate(updateStorage);
+            pstmt.close();
+            connection.close();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+}
